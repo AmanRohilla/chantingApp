3efcc23f2db05673760e8efee0b55b56b6837b01
Aman Rohilla <aman@i2vsys.com>
2020-08-12 01:29:53 +0530

    First Commit

---
+++ app/src/main/java/com/example/mantra/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.mantra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button startButton;
+    RadioButton radioButton;
+    RadioGroup radioGroup;
+    int chantCount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startButton = findViewById(R.id.startButton);
+        radioGroup = findViewById(R.id.radioGroup);
+        
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+                radioButton = (RadioButton) findViewById(selectedId);
+                if(selectedId == -1){
+                    Toast.makeText(getApplicationContext(), "Select something", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    if(radioButton.getId() == findViewById(R.id.letMeDecide).getId()){
+                        showUserSelectionAlertBox();
+                    }
+                    else if(radioButton.getId() == findViewById(R.id.noob).getId()){
+                        chantCount = 11;
+                        goToNextActivity(chantCount);
+                    }
+                    else if(radioButton.getId() == findViewById(R.id.medium).getId()){
+                        chantCount = 21;
+                        goToNextActivity(chantCount);
+                    }
+                    else if(radioButton.getId() == findViewById(R.id.average).getId()){
+                        chantCount = 51;
+                        goToNextActivity(chantCount);
+                    }
+                    else if(radioButton.getId() == findViewById(R.id.aboveAverage).getId()){
+                        chantCount = 101;
+                        goToNextActivity(chantCount);
+                    }
+                }
+            }
+        });
+    }
+
+    void goToNextActivity(int chantCount){
+        Bundle bundle = new Bundle();
+        Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+        intent.putExtra("chantCount",chantCount);
+        startActivity(intent);
+    }
+
+    void showUserSelectionAlertBox(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Enter Number Of Times You Wanna Chant.");
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_NUMBER);
+        builder.setView(input);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                chantCount = Integer.parseInt(input.getText().toString());
+                goToNextActivity(chantCount);
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+    public void onBackPressed() {
+        moveTaskToBack(true);
+    }
+}
---
+++ app/src/main/java/com/example/mantra/Splash_Screen.java
@@ -0,0 +1,31 @@
+package com.example.mantra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    private static int splashScreenTimeout = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); // to cover the entire screen region
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash_Screen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },splashScreenTimeout);
+
+
+    }
+}
